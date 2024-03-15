3732af2adb8e55bc0b7471a6072dc59ea0fca5bb
geogremiller <geogremiller17@gmail.com>
2024-03-15 16:52:03 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isCorrectGuess = false;
+            Random random = new Random();
+
+            int randomNum = random.Next(1, 11);
+            Console.WriteLine("Welcome to numeber guessing game");
+            Console.WriteLine("A number between 1-10 will be generated");
+            Console.WriteLine("If u guess the correct number then u won");
+
+            while (!isCorrectGuess == true)
+            {
+                Console.WriteLine("Please enter your guess: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > randomNum)
+                {
+                    Console.WriteLine("Your guess is too high, try again!");
+                }else if (guess < randomNum)
+                {
+                    Console.WriteLine("Your guess is too low, try again");
+                }else
+                {
+                    Console.WriteLine("Correct");
+                    isCorrectGuess = true;
+                }
+            }
+            Console.WriteLine("Congratulation! You have won the game!");
+            Console.ReadKey();
+        }
+    }
+}
